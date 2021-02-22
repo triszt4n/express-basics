a0c230e64ee121a73b79c6ad271e56fc4ed1bd29
triszt4n <trisztanpiller@gmail.com>
2021-02-22 18:01:50 +0100

    Static pages

--- app/server.ts
+++ app/server.ts
@@ -5,6 +5,7 @@ const app: express.Application = express();
 const port: number = 3000;
 
 app.use('/welcome', WelcomeController);
+app.use(express.static('public'))
 
 // Serve the application at the given port
 app.listen(port, () => {
