85bc1b5acb2b3f41a26805b9b994f40434c715a3
triszt4n <trisztanpiller@gmail.com>
2021-02-24 20:50:55 +0100

    Ready for check-up

--- app/controllers/welcome.controller.ts
+++ app/controllers/welcome.controller.ts
@@ -3,13 +3,15 @@ import { Router, Request, Response } from 'express';
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  // Reply with a hello world when no name param is provided
-  res.send('Hello, World!');
+  res.render('welcome/index', {
+    name: 'World'
+  })
 });
 
-router.get('/:name', (req: Request, res: Response) => {
-  let { name } = req.params;
-  res.send(`Hello, ${name}`);
+router.get('/hello/:name', (req: Request, res: Response) => {
+  res.render('welcome/index', {
+    name: req.params.name
+  })
 });
 
 export const WelcomeController: Router = router;
